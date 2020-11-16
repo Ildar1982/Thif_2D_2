a4f56e32e9fdc289137e2ee30f0a04bbf5d99ed6
Ildar1982 <68797569+Ildar1982@users.noreply.github.com>
2020-11-16 19:37:00 +0500

    init_commit

---
+++ Assets/Scripts/AlarmStart.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class AlarmStart : MonoBehaviour
+{
+    [SerializeField] private GameObject _cameraDinamic;
+    [SerializeField] private AudioSource _audioSourse;
+    [SerializeField] private float _speedAudio;
+
+    private Collider2D _collider2D;
+    private bool _activAlarm = false;
+
+    private void Start()
+    {
+        _audioSourse.volume = 0;
+        _collider2D = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Thif>(out Thif thif))
+        {
+            _activAlarm = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Thif>(out Thif thif))
+        {
+            thif.alarmMove = true;
+            _activAlarm = false;
+            _collider2D.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (_activAlarm == true)
+        {
+            _audioSourse.volume += _speedAudio * Time.deltaTime;
+            _cameraDinamic.SetActive(true);
+        }
+        else
+        {
+            _cameraDinamic.SetActive(false);
+            _audioSourse.volume -= _speedAudio * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Thif.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Thif : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    public bool alarmMove = false;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (alarmMove == false)
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            _animator.SetTrigger("miror");
+            transform.Translate(_speed * Time.deltaTime * -1 * 2, 0, 0);
+        }
+    }
+}
